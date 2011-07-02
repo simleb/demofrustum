ae6a6c9505c342ef70671f7ff19933923bff5318
Simon Leblanc <sleblanc@princeton.edu>
2011-07-01 21:36:23 -0400

    Minor refactoring.

--- Controller.cpp
+++ Controller.cpp
@@ -48,7 +48,7 @@ Controller::Controller() : m_axe(false), m_bounds(true), m_znear(2), m_zfar(10),
 
 void Controller::createScene()
 {
-    m_inside_cam = m_smgr->addCameraSceneNode(0, irr::core::vector3df(0,0,0), irr::core::vector3df(0,0,5));
+    m_inside_cam = m_smgr->addCameraSceneNode(0, irr::core::vector3df(0,0,0), irr::core::vector3df(0,0,m_zfar));
 
     const irr::core::vector3df cube_position(0, 0, 0.5 * (m_zfar - m_znear));
     m_cube = m_smgr->addCubeSceneNode(1, 0, -1, cube_position, irr::core::vector3df(-36,28,0));
@@ -60,8 +60,7 @@ void Controller::createScene()
 
     setFrustum();
     const irr::scene::IGeometryCreator* geo = m_smgr->getGeometryCreator();
-    irr::scene::IMesh* near_mesh = geo->createPlaneMesh(m_near_size, irr::core::dimension2du(1,1), 0,
-                                                        irr::core::dimension2df(1,1));
+    irr::scene::IMesh* near_mesh = geo->createPlaneMesh(m_near_size, irr::core::dimension2du(1,1), 0, irr::core::dimension2df(1,1));
     m_smgr->getMeshManipulator()->setVertexColorAlpha(near_mesh, 30);
     m_near = m_smgr->addMeshSceneNode(near_mesh, 0, -1, m_near_center, irr::core::vector3df(-90,0,0));
     m_near->setMaterialType(irr::video::EMT_TRANSPARENT_VERTEX_ALPHA);
@@ -70,8 +69,7 @@ void Controller::createScene()
     m_near->setVisible(false);
 
     const irr::f32 r = m_zfar / m_znear;
-    irr::scene::IMesh* far_mesh = geo->createPlaneMesh(m_near_size * r, irr::core::dimension2du(1,1), 0,
-                                                       irr::core::dimension2df(1,1));
+    irr::scene::IMesh* far_mesh = geo->createPlaneMesh(m_near_size * r, irr::core::dimension2du(1,1), 0, irr::core::dimension2df(1,1));
     m_smgr->getMeshManipulator()->setVertexColorAlpha(far_mesh, 30);
     m_far = m_smgr->addMeshSceneNode(far_mesh, 0, -1, m_near_center * r, irr::core::vector3df(-90,0,0));
     m_far->setMaterialType(irr::video::EMT_TRANSPARENT_VERTEX_ALPHA);
@@ -82,9 +80,12 @@ void Controller::createScene()
     m_position[OUTSIDE_CAM] = irr::core::vector3df(-8,3,-4);
     m_position[TOP_CAM] = irr::core::vector3df(0,10,5);
     m_position[SIDE_CAM] = irr::core::vector3df(-10,0,5);
+    m_position[OUTSIDE_CAM] = irr::core::vector3df(-0.8, 0.3, -0.4) * m_zfar;
+    m_position[TOP_CAM] = irr::core::vector3df(0, 1 , 0.5) * m_zfar;
+    m_position[SIDE_CAM] = irr::core::vector3df(-1, 0, 0.5) * m_zfar;
 
     m_cam = m_smgr->addCameraSceneNode();
-    m_cam->setTarget(cube_position);
+    m_cam->setTarget(irr::core::vector3df(0, 0, 0.5 * m_zfar));
 }
 
 
@@ -132,27 +133,33 @@ int Controller::run()
 
 void Controller::drawAxe()
 {
-    m_driver->draw3DLine(irr::core::vector3df(0,0,0), irr::core::vector3df(0,0,m_zfar));
+    m_driver->draw3DLine(m_inside_cam->getPosition(), m_inside_cam->getTarget());
 }
 
 
 void Controller::drawBounds()
 {
-    m_driver->draw3DLine(irr::core::vector3df(0,0,0), m_frustum.getFarLeftDown());
-    m_driver->draw3DLine(irr::core::vector3df(0,0,0), m_frustum.getFarLeftUp());
-    m_driver->draw3DLine(irr::core::vector3df(0,0,0), m_frustum.getFarRightUp());
-    m_driver->draw3DLine(irr::core::vector3df(0,0,0), m_frustum.getFarRightDown());
+    const irr::core::vector3df cam = m_inside_cam->getPosition();
+    const irr::core::vector3df fld = m_frustum.getFarLeftDown();
+    const irr::core::vector3df flu = m_frustum.getFarLeftUp();
+    const irr::core::vector3df fru = m_frustum.getFarRightUp();
+    const irr::core::vector3df frd = m_frustum.getFarRightDown();
 
-    m_driver->draw3DLine(m_frustum.getFarLeftDown(),  m_frustum.getFarLeftUp());
-    m_driver->draw3DLine(m_frustum.getFarLeftUp(),    m_frustum.getFarRightUp());
-    m_driver->draw3DLine(m_frustum.getFarRightUp(),   m_frustum.getFarRightDown());
-    m_driver->draw3DLine(m_frustum.getFarRightDown(), m_frustum.getFarLeftDown());
+    m_driver->draw3DLine(cam, fld);
+    m_driver->draw3DLine(cam, flu);
+    m_driver->draw3DLine(cam, fru);
+    m_driver->draw3DLine(cam, frd);
+
+    m_driver->draw3DLine(fld, flu);
+    m_driver->draw3DLine(flu, fru);
+    m_driver->draw3DLine(fru, frd);
+    m_driver->draw3DLine(frd, fld);
 
     const irr::f32 s = m_znear / m_zfar;
-    m_driver->draw3DLine(s * m_frustum.getFarLeftDown(),  s * m_frustum.getFarLeftUp());
-    m_driver->draw3DLine(s * m_frustum.getFarLeftUp(),    s * m_frustum.getFarRightUp());
-    m_driver->draw3DLine(s * m_frustum.getFarRightUp(),   s * m_frustum.getFarRightDown());
-    m_driver->draw3DLine(s * m_frustum.getFarRightDown(), s * m_frustum.getFarLeftDown());
+    m_driver->draw3DLine(s * (fld - cam) + cam, s * (flu - cam) + cam);
+    m_driver->draw3DLine(s * (flu - cam) + cam, s * (fru - cam) + cam);
+    m_driver->draw3DLine(s * (fru - cam) + cam, s * (frd - cam) + cam);
+    m_driver->draw3DLine(s * (frd - cam) + cam, s * (fld - cam) + cam);
 }
 
 

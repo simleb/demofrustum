afa762b933cf91d302e723397a9eeed3b5e96c25
Simon Leblanc <sleblanc@princeton.edu>
2011-07-08 03:17:43 -0400

    Big changes. Simpler, nicer and more intuitive.

--- Controller.cpp
+++ Controller.cpp
@@ -24,7 +24,22 @@
 #include "Controller.h"
 
 
-Controller::Controller() : m_axe(false), m_bounds(true), m_znear(2), m_zfar(10), m_device(0), m_near_center(0,0,m_znear)
+Controller::Controller() : m_mode(TARGET), m_znear(2), m_zfar(10)
+{
+    m_show[  CUBE] = true;
+    m_show[  AXIS] = false;
+    m_show[BOUNDS] = true;
+    m_show[  NEAR] = false;
+    m_show[   FAR] = false;
+    m_show[SCREEN] = false;
+
+    makeDevice();
+    makeScene();
+    initScene();
+}
+
+
+void Controller::makeDevice()
 {
     m_device = irr::createDevice(irr::video::EDT_NULL);
     const irr::s32 n = m_device->getVideoModeList()->getVideoModeCount();
@@ -39,16 +54,15 @@ Controller::Controller() : m_axe(false), m_bounds(true), m_znear(2), m_zfar(10),
     m_driver = m_device->getVideoDriver();
     m_smgr = m_device->getSceneManager();
 
-    const irr::f32 scale_fov = m_znear;
-    m_near_size.set(scale_fov, scale_fov * size.Height / size.Width);
-
-    createScene();
+    const irr::f32 screen_width = 0.5 * (m_znear + m_zfar);
+    m_screen_size.set(screen_width, screen_width * size.Height / size.Width);
 }
 
 
-void Controller::createScene()
+void Controller::makeScene()
 {
-    m_inside_cam = m_smgr->addCameraSceneNode(0, irr::core::vector3df(0,0,0), irr::core::vector3df(0,0,m_zfar));
+    m_inside_cam = m_smgr->addCameraSceneNode();
+    m_cam = m_smgr->addCameraSceneNode();
 
     const irr::core::vector3df cube_position(0, 0, 0.5 * (m_zfar - m_znear));
     m_cube = m_smgr->addCubeSceneNode(1, 0, -1, cube_position, irr::core::vector3df(-36,28,0));
@@ -58,34 +72,38 @@ void Controller::createScene()
     m_cube->setMaterialTexture(0, texture);
     m_cube->setMaterialFlag(irr::video::EMF_LIGHTING, false);
 
-    setFrustum();
     const irr::scene::IGeometryCreator* geo = m_smgr->getGeometryCreator();
-    irr::scene::IMesh* near_mesh = geo->createPlaneMesh(m_near_size, irr::core::dimension2du(1,1), 0, irr::core::dimension2df(1,1));
-    m_smgr->getMeshManipulator()->setVertexColorAlpha(near_mesh, 30);
-    m_near = m_smgr->addMeshSceneNode(near_mesh, 0, -1, m_near_center, irr::core::vector3df(-90,0,0));
-    m_near->setMaterialType(irr::video::EMT_TRANSPARENT_VERTEX_ALPHA);
+    const irr::scene::IMeshManipulator* manip = m_smgr->getMeshManipulator();
+    irr::scene::IMesh* mesh = geo->createPlaneMesh(m_screen_size, irr::core::dimension2du(1,1), 0, irr::core::dimension2df(1,1));
+    manip->setVertexColorAlpha(mesh, 30);
+    manip->transform(mesh, irr::core::matrix4().buildRotateFromTo(irr::core::vector3df(0,1,0), irr::core::vector3df(0,0,1)));
+    m_screen = m_smgr->addMeshSceneNode(mesh, 0, -1, irr::core::vector3df(0, 0, 0.5 * m_zfar));
+    m_screen->setMaterialType(irr::video::EMT_TRANSPARENT_VERTEX_ALPHA);
+    m_screen->getMaterial(0).EmissiveColor.set(255, 255, 192, 0);
+    m_screen->getMaterial(0).BackfaceCulling = false;
+    m_screen->setVisible(false);
+
+    m_near = m_screen->clone();
     m_near->getMaterial(0).EmissiveColor.set(255, 0, 0, 255);
-    m_near->getMaterial(0).BackfaceCulling = false;
-    m_near->setVisible(false);
 
-    const irr::f32 r = m_zfar / m_znear;
-    irr::scene::IMesh* far_mesh = geo->createPlaneMesh(m_near_size * r, irr::core::dimension2du(1,1), 0, irr::core::dimension2df(1,1));
-    m_smgr->getMeshManipulator()->setVertexColorAlpha(far_mesh, 30);
-    m_far = m_smgr->addMeshSceneNode(far_mesh, 0, -1, m_near_center * r, irr::core::vector3df(-90,0,0));
-    m_far->setMaterialType(irr::video::EMT_TRANSPARENT_VERTEX_ALPHA);
+    m_far = m_screen->clone();
     m_far->getMaterial(0).EmissiveColor.set(255, 255, 0, 0);
-    m_far->getMaterial(0).BackfaceCulling = false;
-    m_far->setVisible(false);
+}
 
-    m_position[OUTSIDE_CAM] = irr::core::vector3df(-8,3,-4);
-    m_position[TOP_CAM] = irr::core::vector3df(0,10,5);
-    m_position[SIDE_CAM] = irr::core::vector3df(-10,0,5);
-    m_position[OUTSIDE_CAM] = irr::core::vector3df(-0.8, 0.3, -0.4) * m_zfar;
-    m_position[TOP_CAM] = irr::core::vector3df(0, 1 , 0.5) * m_zfar;
-    m_position[SIDE_CAM] = irr::core::vector3df(-1, 0, 0.5) * m_zfar;
 
-    m_cam = m_smgr->addCameraSceneNode();
-    m_cam->setTarget(irr::core::vector3df(0, 0, 0.5 * m_zfar));
+void Controller::initScene()
+{
+    m_position[OUTSIDE] = irr::core::vector3df(-0.8, 0.3, -0.4) * m_zfar;
+    m_position[TOP] = irr::core::vector3df(0, 1 , 0.5) * m_zfar;
+    m_position[SIDE] = irr::core::vector3df(-1, 0, 0.5) * m_zfar;
+
+    m_inside_cam->setPosition(irr::core::vector3df(0, 0, 0));
+    m_inside_cam->setTarget(irr::core::vector3df(0, 0, m_zfar));
+
+    m_cam->setPosition(irr::core::vector3df(0, 0, 0));
+    m_cam->setTarget(m_screen->getPosition());
+
+    m_cube->setPosition(irr::core::vector3df(0, 0, 0.5 * (m_zfar - m_znear)));
 }
 
 
@@ -98,26 +116,10 @@ int Controller::run()
         else
             m_smgr->setActiveCamera(m_cam);
 
-        setFrustum();
-
-        const irr::core::vector3df pos(-m_near_center.X,-m_near_center.Y,m_near_center.Z);
-        m_near->setPosition(pos);
-        m_far->setPosition(pos * m_zfar / m_znear);
-
-        irr::video::SMaterial mat;
-        mat.setFlag(irr::video::EMF_LIGHTING, false);
-        m_driver->setMaterial(mat);
-
-        m_driver->setTransform(irr::video::ETS_WORLD, irr::core::IdentityMatrix);
-
         m_driver->beginScene(true, true, 0);
 
-        if (m_bounds)
-            drawBounds();
-
-        if (m_axe)
-            drawAxe();
-
+        drawScene();
+        m_inside_cam->render();
         m_smgr->drawAll();
 
         m_driver->endScene();
@@ -131,43 +133,95 @@ int Controller::run()
 }
 
 
-void Controller::drawAxe()
-{
-    m_driver->draw3DLine(m_inside_cam->getPosition(), m_inside_cam->getTarget());
-}
-
-
-void Controller::drawBounds()
+void Controller::drawScene()
 {
     const irr::core::vector3df cam = m_inside_cam->getPosition();
-    const irr::core::vector3df fld = m_frustum.getFarLeftDown();
-    const irr::core::vector3df flu = m_frustum.getFarLeftUp();
-    const irr::core::vector3df fru = m_frustum.getFarRightUp();
-    const irr::core::vector3df frd = m_frustum.getFarRightDown();
-
-    m_driver->draw3DLine(cam, fld);
-    m_driver->draw3DLine(cam, flu);
-    m_driver->draw3DLine(cam, fru);
-    m_driver->draw3DLine(cam, frd);
-
-    m_driver->draw3DLine(fld, flu);
-    m_driver->draw3DLine(flu, fru);
-    m_driver->draw3DLine(fru, frd);
-    m_driver->draw3DLine(frd, fld);
-
+    const irr::core::vector3df screen = m_screen->getPosition();
+    const irr::f32 zscreen = screen.Z - cam.Z;
+    const irr::f32 q = m_znear / zscreen;
+    const irr::f32 r = m_zfar / zscreen;
     const irr::f32 s = m_znear / m_zfar;
-    m_driver->draw3DLine(s * (fld - cam) + cam, s * (flu - cam) + cam);
-    m_driver->draw3DLine(s * (flu - cam) + cam, s * (fru - cam) + cam);
-    m_driver->draw3DLine(s * (fru - cam) + cam, s * (frd - cam) + cam);
-    m_driver->draw3DLine(s * (frd - cam) + cam, s * (fld - cam) + cam);
+    const irr::f32 t = zscreen / m_zfar;
+    const irr::scene::SViewFrustum* frustum = m_inside_cam->getViewFrustum();
+    const irr::core::vector3df fld = frustum->getFarLeftDown();
+    const irr::core::vector3df flu = frustum->getFarLeftUp();
+    const irr::core::vector3df fru = frustum->getFarRightUp();
+    const irr::core::vector3df frd = frustum->getFarRightDown();
+    const irr::core::vector3df fc = 0.25 * (fld + flu + fru + frd);
+
+    updateFrustum();
+
+    m_driver->setTransform(irr::video::ETS_WORLD, irr::core::IdentityMatrix);
+
+    irr::video::SMaterial mat; mat.setFlag(irr::video::EMF_LIGHTING, false);
+    m_driver->setMaterial(mat);
+
+    if (m_show[AXIS])
+        m_driver->draw3DLine(m_inside_cam->getPosition(), m_inside_cam->getTarget());
+
+    if (m_show[BOUNDS])
+    {
+        m_driver->draw3DLine(cam, fld);
+        m_driver->draw3DLine(cam, flu);
+        m_driver->draw3DLine(cam, fru);
+        m_driver->draw3DLine(cam, frd);
+
+        m_driver->draw3DLine(fld, flu);
+        m_driver->draw3DLine(flu, fru);
+        m_driver->draw3DLine(fru, frd);
+        m_driver->draw3DLine(frd, fld);
+
+        m_driver->draw3DLine(s * (fld - cam) + cam, s * (flu - cam) + cam);
+        m_driver->draw3DLine(s * (flu - cam) + cam, s * (fru - cam) + cam);
+        m_driver->draw3DLine(s * (fru - cam) + cam, s * (frd - cam) + cam);
+        m_driver->draw3DLine(s * (frd - cam) + cam, s * (fld - cam) + cam);
+
+        if (m_show[SCREEN])
+        {
+            m_driver->draw3DLine(t * (fld - cam) + cam, t * (flu - cam) + cam);
+            m_driver->draw3DLine(t * (flu - cam) + cam, t * (fru - cam) + cam);
+            m_driver->draw3DLine(t * (fru - cam) + cam, t * (frd - cam) + cam);
+            m_driver->draw3DLine(t * (frd - cam) + cam, t * (fld - cam) + cam);
+        }
+    }
+
+    if (m_show[NEAR])
+    {
+        m_near->setPosition(s * (fc - cam) + cam);
+        m_near->setScale(irr::core::vector3df(q));
+        if (m_mode == TARGET)
+        {
+            const irr::core::vector3df v = (screen-cam).getSphericalCoordinateAngles();
+            m_near->setRotation(irr::core::vector3df(90+v.X, 90-(v.Y==0?+90:v.Y), 0));
+        }
+        else
+        {
+            m_near->setRotation(irr::core::vector3df(0));
+        }
+    }
+
+    if (m_show[FAR])
+    {
+        m_far->setPosition(fc);
+        m_far->setScale(irr::core::vector3df(r));
+        if (m_mode == TARGET)
+        {
+            const irr::core::vector3df v = (screen-cam).getSphericalCoordinateAngles();
+            m_far->setRotation(irr::core::vector3df(90+v.X, 90-(v.Y==0?+90:v.Y), 0));
+        }
+        else
+        {
+            m_far->setRotation(irr::core::vector3df(0));
+        }
+    }
 }
 
 
-void Controller::switchToCam(CameraId id)
+void Controller::changeCamera(Camera id)
 {
-    m_smgr->setActiveCamera(m_cam);
     irr::scene::ISceneNodeAnimator* anim;
-    anim = m_smgr->createFlyStraightAnimator(m_cam->getPosition(), m_position[id], 300);
+    const irr::core::vector3df new_position = (id == INSIDE ? m_inside_cam->getPosition() : m_position[id]);
+    anim = m_smgr->createFlyStraightAnimator(m_cam->getPosition(), new_position, 300);
     m_cam->addAnimator(anim);
     anim->drop();
 }
@@ -180,32 +234,44 @@ bool Controller::OnEvent(const irr::SEvent& event)
         switch (event.KeyInput.Key)
         {
           case irr::KEY_KEY_1: // Inside cam
-            switchToCam(INSIDE_CAM);
+            changeCamera(INSIDE);
             return true;
           case irr::KEY_KEY_2: // Outide cam
-            switchToCam(OUTSIDE_CAM);
+            changeCamera(OUTSIDE);
             return true;
           case irr::KEY_KEY_3: // Side cam
-            switchToCam(SIDE_CAM);
+            changeCamera(SIDE);
             return true;
           case irr::KEY_KEY_4: // Top cam
-            switchToCam(TOP_CAM);
+            changeCamera(TOP);
             return true;
-          case irr::KEY_KEY_N: // Toggle near plan coloring
-            m_near->setVisible(!m_near->isVisible());
-            return true;
-          case irr::KEY_KEY_F: // Toggle far plan coloring
-            m_far->setVisible(!m_far->isVisible());
-            return true;
-          case irr::KEY_KEY_B: // Toggle bounds
-            m_bounds = !m_bounds;
+          case irr::KEY_KEY_C: // Toggle cube
+            m_show[CUBE] = !m_show[CUBE];
+            m_cube->setVisible(m_show[CUBE]);
             return true;
           case irr::KEY_KEY_A: // Toggle axe
-            m_axe = !m_axe;
+            m_show[AXIS] = !m_show[AXIS];
+            return true;
+          case irr::KEY_KEY_B: // Toggle bounds
+            m_show[BOUNDS] = !m_show[BOUNDS];
+            return true;
+          case irr::KEY_KEY_N: // Toggle near plan coloring
+            m_show[NEAR] = !m_show[NEAR];
+            m_near->setVisible(m_show[NEAR]);
+            return true;
+          case irr::KEY_KEY_F: // Toggle far plan coloring
+            m_show[FAR] = !m_show[FAR];
+            m_far->setVisible(m_show[FAR]);
+            return true;
+          case irr::KEY_KEY_S: // Toggle screen plan coloring
+            m_show[SCREEN] = !m_show[SCREEN];
+            m_screen->setVisible(m_show[SCREEN]);
             return true;
           case irr::KEY_KEY_R: // Reset position
-            m_near_center.set(0, 0, m_znear);
-            m_cube->setPosition(irr::core::vector3df(0 , 0, 0.5 * (m_zfar - m_znear)));
+            initScene();
+            return true;
+          case irr::KEY_TAB: // Switch camera behavior mode
+            m_mode = (m_mode + 1) % MODE_COUNT;
             return true;
           case irr::KEY_ESCAPE: // Quit
             m_device->closeDevice();
@@ -214,22 +280,53 @@ bool Controller::OnEvent(const irr::SEvent& event)
             return false;
         }
     }
-    if (event.EventType == irr::EET_KEY_INPUT_EVENT && event.KeyInput.PressedDown && !event.KeyInput.Shift) // Move cube
+    if (event.EventType == irr::EET_KEY_INPUT_EVENT && event.KeyInput.PressedDown && !event.KeyInput.Shift) // Move inside camera
     {
-        const irr::f32 step = 0.2;
+        const irr::f32 step = 0.05;
+        const irr::core::vector3df position = m_inside_cam->getPosition();
         switch (event.KeyInput.Key)
         {
+          case irr::KEY_LEFT:
+            m_inside_cam->setPosition(position - irr::core::vector3df(step,0,0));
+            break;
+          case irr::KEY_RIGHT:
+            m_inside_cam->setPosition(position + irr::core::vector3df(step,0,0));
+            break;
           case irr::KEY_UP:
-            m_cube->setPosition(m_cube->getPosition() + irr::core::vector3df(0,step,0));
-            return true;
+            m_inside_cam->setPosition(position + irr::core::vector3df(0,step,0));
+            break;
           case irr::KEY_DOWN:
-            m_cube->setPosition(m_cube->getPosition() - irr::core::vector3df(0,step,0));
-            return true;
+            m_inside_cam->setPosition(position - irr::core::vector3df(0,step,0));
+            break;
+          case irr::KEY_KEY_P:
+            m_inside_cam->setPosition(position + irr::core::vector3df(0,0,step));
+            break;
+          case irr::KEY_KEY_M:
+            m_inside_cam->setPosition(position - irr::core::vector3df(0,0,step));
+            break;
+          default:
+            return false;
+        }
+        m_cam->removeAnimators();
+        m_cam->setPosition(m_smgr->getActiveCamera()->getPosition());
+        return true;
+    }
+    if (event.EventType == irr::EET_KEY_INPUT_EVENT && event.KeyInput.PressedDown && event.KeyInput.Shift) // Move cube
+    {
+        const irr::f32 step = 0.1;
+        switch (event.KeyInput.Key)
+        {
           case irr::KEY_LEFT:
             m_cube->setPosition(m_cube->getPosition() - irr::core::vector3df(step,0,0));
             return true;
           case irr::KEY_RIGHT:
             m_cube->setPosition(m_cube->getPosition() + irr::core::vector3df(step,0,0));
+            return true;
+          case irr::KEY_UP:
+            m_cube->setPosition(m_cube->getPosition() + irr::core::vector3df(0,step,0));
+            return true;
+          case irr::KEY_DOWN:
+            m_cube->setPosition(m_cube->getPosition() - irr::core::vector3df(0,step,0));
             return true;
           case irr::KEY_KEY_P:
             m_cube->setPosition(m_cube->getPosition() + irr::core::vector3df(0,0,step));
@@ -241,54 +338,41 @@ bool Controller::OnEvent(const irr::SEvent& event)
             return false;
         }
     }
-    if (event.EventType == irr::EET_KEY_INPUT_EVENT && event.KeyInput.PressedDown && event.KeyInput.Shift) // Move near plane
-    {
-        const irr::f32 step = 0.05;
-        switch (event.KeyInput.Key)
-        {
-          case irr::KEY_UP:
-            m_near_center -= irr::core::vector3df(0,step,0);
-            return true;
-          case irr::KEY_DOWN:
-            m_near_center += irr::core::vector3df(0,step,0);
-            return true;
-          case irr::KEY_LEFT:
-            m_near_center += irr::core::vector3df(step,0,0);
-            return true;
-          case irr::KEY_RIGHT:
-            m_near_center -= irr::core::vector3df(step,0,0);
-            return true;
-          default:
-            return false;
-        }
-    }
     return false;
 }
 
 
-void Controller::setFrustum()
+void Controller::updateFrustum()
 {
-    const irr::f32 xmin = m_near_center.X - 0.5 * m_near_size.Width;
-    const irr::f32 xmax = m_near_center.X + 0.5 * m_near_size.Width;
-    const irr::f32 ymin = m_near_center.Y - 0.5 * m_near_size.Height;
-    const irr::f32 ymax = m_near_center.Y + 0.5 * m_near_size.Height;
-
+    const irr::core::vector3df cam = m_inside_cam->getPosition();
+    const irr::core::vector3df screen = m_screen->getPosition();
+    const irr::f32 zscreen = screen.Z - cam.Z;
+    const irr::f32 q = m_znear / zscreen;
     irr::core::matrix4 proj;
-    proj[0] = (2.0 * m_znear) / (xmax - xmin);
-    proj[5] = (2.0 * m_znear) / (ymax - ymin);
-    proj[8] = (xmax + xmin) / (xmax - xmin);
-    proj[9] = (ymax + ymin) / (ymax - ymin);
-    proj[10] = (m_zfar + m_znear) / (m_zfar - m_znear);
-    proj[11] = 1.0;
-    proj[14] = -(2.0 * m_zfar * m_znear) / (m_zfar - m_znear);
-    proj[15] = 0.0;
-    m_inside_cam->setProjectionMatrix(proj);
 
-    m_frustum = irr::scene::SViewFrustum(*m_inside_cam->getViewFrustum());
-    m_frustum.cameraPosition = m_inside_cam->getAbsolutePosition();
-    irr::core::matrix4 m(irr::core::matrix4::EM4CONST_NOTHING);
-    m.setbyproduct_nocheck(m_frustum.getTransform(irr::video::ETS_PROJECTION),
-                           m_frustum.getTransform(irr::video::ETS_VIEW));
-    m_frustum.setFrom(m);
+    if (m_mode == TARGET)
+    {
+        proj.buildProjectionMatrixPerspectiveLH(q * m_screen_size.Width, q * m_screen_size.Height, m_znear, m_zfar);
+        m_inside_cam->setProjectionMatrix(proj);
+        m_inside_cam->setTarget(screen);
+    }
+    if (m_mode == FRUSTUM)
+    {
+        const irr::f32 xmin = q * (cam.X - screen.X - 0.5 * m_screen_size.Width);
+        const irr::f32 xmax = q * (cam.X - screen.X + 0.5 * m_screen_size.Width);
+        const irr::f32 ymin = q * (cam.Y - screen.Y - 0.5 * m_screen_size.Height);
+        const irr::f32 ymax = q * (cam.Y - screen.Y + 0.5 * m_screen_size.Height);
+
+        proj[0] = (2.0 * m_znear) / (xmax - xmin);
+        proj[5] = (2.0 * m_znear) / (ymax - ymin);
+        proj[8] = (xmax + xmin) / (xmax - xmin);
+        proj[9] = (ymax + ymin) / (ymax - ymin);
+        proj[10] = (m_zfar + m_znear) / (m_zfar - m_znear);
+        proj[11] = 1.0;
+        proj[14] = -(2.0 * m_zfar * m_znear) / (m_zfar - m_znear);
+        proj[15] = 0.0;
+        m_inside_cam->setProjectionMatrix(proj);
+        m_inside_cam->setTarget(cam + irr::core::vector3df(0, 0, m_zfar));
+    }
 }
 
--- Controller.h
+++ Controller.h
@@ -26,43 +26,48 @@
 #include <irrlicht.h>
 
 
-enum CameraId { INSIDE_CAM, OUTSIDE_CAM, SIDE_CAM, TOP_CAM, CAM_COUNT };
+enum Mode { TARGET, FRUSTUM, MODE_COUNT };
+enum Camera { INSIDE, OUTSIDE, SIDE, TOP, CAM_COUNT };
+enum Element { CUBE, AXIS, BOUNDS, NEAR, FAR, SCREEN, OBJECT_COUNT };
 
 
 class Controller : public irr::IEventReceiver
 {
   public:
     Controller();
-    void createScene();
+    void makeDevice();
+    void makeScene();
+
+    void initScene();
+    void drawScene();
+
+    void changeCamera(Camera id);
+    void updateFrustum();
 
     int run();
 
-    void drawAxe();
-    void drawBounds();
-
-    void switchToCam(CameraId id);
-
     virtual bool OnEvent(const irr::SEvent& event);
 
-    void setFrustum();
-
   protected:
-    bool m_axe;
-    bool m_bounds;
+    bool m_show[OBJECT_COUNT];
+    irr::u32 m_mode;
+
+    irr::scene::ICameraSceneNode* m_inside_cam;
     irr::f32 m_znear;
     irr::f32 m_zfar;
+
+    irr::scene::ICameraSceneNode* m_cam;
+    irr::core::vector3df m_position[CAM_COUNT];
+
     irr::IrrlichtDevice* m_device;
     irr::video::IVideoDriver* m_driver;
     irr::scene::ISceneManager* m_smgr;
-    irr::scene::ICameraSceneNode* m_inside_cam;
-    irr::scene::ICameraSceneNode* m_cam;
-    irr::core::vector3df m_position[CAM_COUNT];
+
     irr::scene::ISceneNode* m_cube;
-    irr::scene::SViewFrustum m_frustum;
-    irr::scene::IMeshSceneNode* m_near;
-    irr::scene::IMeshSceneNode* m_far;
-    irr::core::vector3df m_near_center;
-    irr::core::dimension2df m_near_size;
+    irr::scene::ISceneNode* m_near;
+    irr::scene::ISceneNode* m_far;
+    irr::scene::ISceneNode* m_screen;
+    irr::core::dimension2df m_screen_size;
 };
 
 #endif
